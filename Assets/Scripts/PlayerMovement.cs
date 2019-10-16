f18151cfb345bd1a986342bcb4b91ef86fa175ca
Temi <mostovoyart@gmail.com>
2019-10-16 22:56:45 +0300

    wow, i finaly upload this thing

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 8;
+    public Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(move * speed);
+    }
+}
